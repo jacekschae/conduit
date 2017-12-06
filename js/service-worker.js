df766eb478b1c94a2e505196cb68179543ea9333
Jacek Schae <jacek.schae@gmail.com>
2017-12-06 11:47:33 +0100

    service-worker #11

--- js/service-worker.js
+++ js/service-worker.js
@@ -14,7 +14,6 @@ function setOfCachedUrls(e) {
 var precacheConfig = [
     ['../index.html', 'cab2aa407a4e4df733953e4e8aad92f7'],
     ['./client.js', '302476b8b379a677f648aa1e48918ebd'],
-    ['../manifest.json', 'oMwLXNx6fbAxB29kzBk6RutQVwjX9HTV'],
     ['https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css', 'Bs8oRc3Ra7oBxMT2YdrF2YhKbhqLgfUv'],
     [
       'https://fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic',
