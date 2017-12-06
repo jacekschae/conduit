f3ac7ae851b7a204047455c22305090cf9fa1444
Jacek Schae <jacek.schae@gmail.com>
2017-12-06 10:40:27 +0100

    service-worker #4

--- js/service-worker.js
+++ js/service-worker.js
@@ -11,7 +11,15 @@ function setOfCachedUrls(e) {
       return new Set(e)
     })
 }
-var precacheConfig = [['/index.html', 'cab2aa407a4e4df733953e4e8aad92f7']],
+var precacheConfig = [
+    ['/index.html', 'cab2aa407a4e4df733953e4e8aad92f7'],
+    ['/js/client.js', 'gcb1ab395a3sh5f192847e4e8add9gf4'],
+    ['https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css', '302476b8b379a677f648aa1e48918ebd'],
+    [
+      'https://fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic',
+      'a2595ebcedc2c0f70756d2fdf1f13029'
+    ]
+  ],
   cacheName = 'sw-precache-v3-sw-precache-webpack-plugin-' + (self.registration ? self.registration.scope : ''),
   ignoreUrlParametersMatching = [/^utm_/],
   addDirectoryIndex = function(e, t) {
